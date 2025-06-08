905099fb21ece51d167648565f882f3ca31fe4a3
fish3er <167292998+fish3er@users.noreply.github.com>
2025-06-08 09:36:13 +0200

    przykłady

---
+++ generatorexamples.py
@@ -0,0 +1,32 @@
+
+def square(x):
+    result = []
+    for i in x:
+        result.append(i * i)
+    return result
+
+def square_gen(x):
+    for i in x:
+        yield i * i
+
+nums_lit = [1, 2, 3, 4, 5]
+
+#list comprehension
+square_comprahension = [x*x for x in nums_lit]
+print(square_comprahension)
+
+# generator expression
+square_gen_expression = (x*x for x in nums_lit)
+print(square_gen_expression)
+print(next(square_gen_expression))
+
+
+# print(square(nums_lit))
+# square_nums = square_gen(nums_lit)
+# print(square_nums)
+# print(next(square_nums))
+# print(next(square_nums))
+# print(next(square_nums))
+# print(next(square_nums))
+# print(next(square_nums))
+# print(next(square_nums))
